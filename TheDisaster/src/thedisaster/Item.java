bf8e37747e67a50c2bade2cc31ac24af5bca7da5
C0116168 <C0116168@DESKTOP-JLJ4J9G>
2018-11-27 19:38:35 +0900

    コメントをいい感じにしました。多分これで完成形だと思います。

--- TheDisaster/src/thedisaster/Item.java
+++ TheDisaster/src/thedisaster/Item.java
@@ -1,39 +1,42 @@
-/*
- * 佐藤瑠星 & ザイダトゥル 共同
- */
 package thedisaster;
+
 /**
+ * 武器・回復アイテムの名前・値を設定・返すクラス
  *
- * @author user
+ * @author 佐藤瑠星 & ザイダトゥル
  */
 public class Item {
+
     private String name; //名前
     private int value; //値
-    
+
     /**
      * コンストラクタ
+     *
      * @param n 名前
      * @param v 値
      */
     public Item(String n, int v) {
-        this.name = n; 
+        this.name = n;
         this.value = v;
     }
-    
+
     /**
      * アイテムの名前を返すメソッド
+     *
      * @return 名前
      */
-    public String GetName(){
+    public String GetName() {
         return name;
     }
 
     /**
      * アイテムが保有する値を返すメソッド
-     * @return 
+     *
+     * @return 値
      */
-    public int GetValue(){
+    public int GetValue() {
         return value;
     }
-    
+
 }
