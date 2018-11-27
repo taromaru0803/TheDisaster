826d2dff5f11f245300ae0c333931a306b70ddaa
C0116168 <C0116168@DESKTOP-JLJ4J9G>
2018-11-27 19:35:52 +0900

    戦闘と特殊勝利ルート実装。動きます。

--- TheDisaster/src/thedisaster/TheDisaster.java
+++ TheDisaster/src/thedisaster/TheDisaster.java
@@ -1,14 +1,14 @@
 package thedisaster;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * メインのファイル。
+ * メインのファイル
+ *
+ * @author チーム16全員 (主に立野が担当)
  */
 public class TheDisaster {
 
@@ -16,79 +16,150 @@ public class TheDisaster {
     static Character Disaster;
     static Grim grimgerde;
 
-    static List<Dungeon> dungeonList = new ArrayList<Dungeon>();
+    static Grim enemy_tutorial_1;
+    static Grim enemy_tutorial_2;
+    static Grim[] enemies_tutorial = new Grim[2];
+    static Grim enemy_stage1_1;
+    static Grim enemy_stage1_2;
+    static Grim[] enemies_stage1 = new Grim[2];
+    static Grim enemy_stage2_1;
+    static Grim enemy_stage2_2;
+    static Grim[] enemies_stage2 = new Grim[2];
+    static Grim enemy_stage3_1;
+    static Grim enemy_stage3_2;
+    static Grim enemy_stage3_3;
+    static Grim enemy_stage3_4;
+    static Grim[] enemies_stage3 = new Grim[4];
 
     static Item heal_low;
     static Item heal_medium;
     static Item heal_high;
     static Item heal_veryHigh;
 
-    static Item wepon_knife;
-    static Item wepon_toyKnife;
-    static Item wepon_FryingPanOfMagic;
-    static Item wepon_theElderWand;
-    static Item wepon_muskets;
-    static Item wepon_waterGun;
+    static Item weapon_bareHands;
+    static Item weapon_knife;
+    static Item weapon_toyKnife;
+    static Item weapon_fryingPanOfMagic;
+    static Item weapon_theElderWand;
+    static Item weapon_muskets;
+    static Item weapon_waterGun;
+
+    static boolean hasGotWeapon_toyKnife = false;
+    static boolean hasGotWeapon_fryingPanOfMagic = false;
+    static boolean hasGotWeapon_waterGun = false;
+    static boolean hasGotWeapon_killWeapon = false;
 
     static Scanner scan = new Scanner(System.in);
 
+    static boolean isGameEnd = false;
+
+    /**
+     * メインメソッド。全体の流れはここに記述されています。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         init(); //初期化
+
         Story.Openning(); //オープニング
         PutDisasterName(); //名前決定
         DamageOverTime(); //HP減少開始
-        
+
         Story.Tutorial(); //チュートリアル
+        Battle.BattleOfMessiah(enemies_tutorial); //チュートリアル戦
         Story.TutorialBattleAfter(); //チュートリアル戦後
-        
+
         Story.choice1(); //選択1
+
         //分岐 810 or ステージ1
-        
+        if (TwoChoices("1", "2").equals("1")) {
+            EquipWeapon(weapon_knife);
+        } else {
+            Healer(heal_low);
+            Story.specialenpai();
+        }
+
         Story.stage1(); //ステージ1
+        Battle.BattleOfMessiah(enemies_stage1); //ステージ1戦
         Story.stage1After(); //ステージ1戦後
+
         //分岐 ステージ2 or choice2
-        
+        if (TwoChoices("1", "2").equals("1")) {
+            Story.stage2();
+            Battle.BattleOfMessiah(enemies_stage2);
+            Story.stage2After();
+        } else {
+            Story.choice2();
+        }
+
         Story.choice3(); //choice3
-        
+
         Story.stage3(); //ステージ3
+        Battle.BattleOfMessiah(enemies_stage3); //ステージ3戦
         Story.stage3After(); //ステージ3戦後
-        
+
         Story.security(); //イージマス前
         EasyMass(); //イージマス
-        
+
         Story.lastBattle(); //ラスト前
+        if (hasGotWeapon_toyKnife
+                && hasGotWeapon_fryingPanOfMagic
+                && hasGotWeapon_waterGun
+                && !hasGotWeapon_killWeapon) {
+            System.out.println("1.戦う , 2. 自分の命を絶つ");
+            if (TwoChoices("1", "2").equals("2")) {
+                Story.specialVictory();
+                isGameEnd = true;
+            }
+        }
+        Battle.BattleOfGrimgerde();
         Story.lastBattleAfter(); //ラスト後
-        
+
         Story.epilogue(); //エンディング
+
+        isGameEnd = true;
     }
 
+    /**
+     * 初期化
+     */
     private static void init() {
-
         heal_low = new Item("小回復", 20);
         heal_medium = new Item("中回復", 40);
         heal_high = new Item("大回復", 60);
         heal_veryHigh = new Item("特大回復", 200);
 
+        weapon_bareHands = new Item("素手", 0);
+        weapon_knife = new Item("ナイフ", 10);
+        weapon_toyKnife = new Item("おもちゃのナイフ", 10);
+        weapon_fryingPanOfMagic = new Item("魔法のフライパン", 10);
+        weapon_theElderWand = new Item("ニワトコの杖", 10);
+        weapon_muskets = new Item("マスケット銃", 10);
+        weapon_waterGun = new Item("水鉄砲", 10);
+
+        Disaster = new Character("", 1500, weapon_bareHands);
         grimgerde = new Grim("Grimgerde", 10000);
 
-        wepon_knife = new Item("ナイフ", 10);
-        wepon_toyKnife = new Item("おもちゃのナイフ", 10);
-        wepon_FryingPanOfMagic = new Item("魔法のフライパン", 10);
-        wepon_theElderWand = new Item("ニワトコの杖", 10);
-        wepon_muskets = new Item("マスケット銃", 10);
-        wepon_waterGun = new Item("水鉄砲", 10);
-
-        dungeonList.add(new Dungeon("チュートリアル", 2, "", "", ""));
-        dungeonList.add(new Dungeon("選択1", 0, "", "", wepon_knife.GetName()));
-        dungeonList.add(new Dungeon("810", 0, "", "", wepon_toyKnife.GetName()));
-        dungeonList.add(new Dungeon("ステージ1", 2, "小", "", ""));
-        dungeonList.add(new Dungeon("SP", 0, "", "中", "魔法のフライパン"));
-        dungeonList.add(new Dungeon("ステージ2", 2, "中", "", "ニワトコの杖"));
-        dungeonList.add(new Dungeon("選択2", 0, "", "大", "マスケット銃"));
-        dungeonList.add(new Dungeon("ステージ3", 4, "特大", "", "水鉄砲"));
-        dungeonList.add(new Dungeon("イージーマス", 0, "", "", ""));
-        dungeonList.add(new Dungeon("ラスボス", 1, "", "", ""));
-
+        enemy_tutorial_1 = new Grim("メサイア一般兵A", 100);
+        enemy_tutorial_2 = new Grim("メサイア一般兵B", 100);
+        enemies_tutorial[0] = enemy_tutorial_1;
+        enemies_tutorial[1] = enemy_tutorial_2;
+        enemy_stage1_1 = new Grim("エクスシア", 250);
+        enemy_stage1_2 = new Grim("スローネ", 250);
+        enemies_stage1[0] = enemy_stage1_1;
+        enemies_stage1[1] = enemy_stage1_2;
+        enemy_stage2_1 = new Grim("マールート", 500);
+        enemy_stage2_2 = new Grim("ハールート", 500);
+        enemies_stage2[0] = enemy_stage2_1;
+        enemies_stage2[1] = enemy_stage2_2;
+        enemy_stage3_1 = new Grim("ラジエル", 1000);
+        enemy_stage3_2 = new Grim("ヴァーチュース", 1000);
+        enemy_stage3_3 = new Grim("ガルム", 1000);
+        enemy_stage3_4 = new Grim("アストライアー", 2000);
+        enemies_stage3[0] = enemy_stage3_1;
+        enemies_stage3[1] = enemy_stage3_2;
+        enemies_stage3[2] = enemy_stage3_3;
+        enemies_stage3[3] = enemy_stage3_4;
     }
 
     /**
@@ -109,9 +180,12 @@ public class TheDisaster {
                 roop = true;
             }
         } while (roop);
-        Disaster = new Character(DisasterName, 2000, "なし");
+        Disaster.SetName(DisasterName);
     }
 
+    /**
+     * イージーマス
+     */
     public static void EasyMass() {
         System.out.println(""
                 + "※簡単な数学の問題が5問表示されます\n"
@@ -131,13 +205,12 @@ public class TheDisaster {
         int sisoku = 0;//四則種類選択用
         String question = null;//問題内容保存用
         char loop = 0;//繰り返し回数保存用
-        char i;//繰り返し回数一時保存用
-        do {//do/while文で３問繰り返す
-            for (i = 0; i < 2; i++) {//２つの数値を保存
-                int q = rand.nextInt(12);
+        do {//do/while文で5問繰り返す
+            for (int i = 0; i < 2; i++) {//２つの数値を保存
+                int q = rand.nextInt(13);
                 num[i] = q;
             }
-            sisoku = rand.nextInt(3);//ランダムに＋ー×÷を選択
+            sisoku = rand.nextInt(3);//ランダムに＋ー×を選択
             switch (sisoku) {
                 case 0:
                     ans = num[0] + num[1];
@@ -180,17 +253,38 @@ public class TheDisaster {
      * @param wepon 武器
      */
     public static void EquipWeapon(Item wepon) {
-        System.out.println(wepon.GetName() + "を装備しますか? (y/n) (現在の装備 : " + Disaster.getWepon() + ")");
+        String weponName = wepon.GetName();
+
+        System.out.println(weponName + "を装備しますか? (y/n) (現在の装備 : " + Disaster.getWepon().GetName() + ")");
 
         if (TwoChoices("y", "n").equals("y")) {
-            System.out.println(wepon.GetName() + "を装備しました▼");
-            Disaster.SetWepon(wepon.GetName());
+            System.out.println(weponName + "を装備しました▼");
+            Disaster.SetWepon(wepon);
+
+            if (!hasGotWeapon_killWeapon) {
+                if (weponName.equals(weapon_toyKnife.GetName())) {
+                    hasGotWeapon_toyKnife = true;
+                } else if (weponName.equals(weapon_fryingPanOfMagic.GetName())) {
+                    hasGotWeapon_fryingPanOfMagic = true;
+                } else if (weponName.equals(weapon_waterGun.GetName())) {
+                    hasGotWeapon_waterGun = true;
+                } else {
+                    hasGotWeapon_killWeapon = true;
+                }
+            }
+            
         } else {
             System.out.println(wepon.GetName() + "を装備しませんでした▼");
         }
+        
         scan.nextLine();
     }
 
+    /**
+     * 回復の実行(確認)をするメソッド
+     *
+     * @param healItem 回復アイテム
+     */
     public static void Healer(Item healItem) {
         System.out.println("体力を回復しますか? (y/n) (現在の体力 : " + Disaster.getHP() + ")");
 
@@ -242,6 +336,11 @@ public class TheDisaster {
                 Disaster.SetHP(-1);
                 //System.out.println("HP: " + Disaster.getHP());
 
+                if (isGameEnd) {
+                    timer.cancel();
+                    System.exit(0);
+                }
+
                 if (Disaster.getHP() <= 0) {
                     timer.cancel();
                     DisasterDead();
@@ -251,6 +350,9 @@ public class TheDisaster {
         timer.schedule(task, 0, 1000);
     }
 
+    /**
+     * Disasterの死亡処理を行うメソッド
+     */
     public static void DisasterDead() {
         //scan.nextline()の処理の途中でscan.close()が入ると、nextlineの処理終了後にcloseするようです。
         //scan.close();
